cef533f4b6fbba1c7ce32be3f28fe8a4739cb43a
Dominic Amato <dominic.a.amato@gmail.com>
2019-11-06 12:38:09 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CSSH",
+    pkgConfig: "libssh2",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
